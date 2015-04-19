fad9bf0624680751d8f629665aa7a7ba0ce285de
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-04-19 16:07:04 -0700

    Update time.h

--- time.h
+++ time.h
@@ -8,14 +8,26 @@ class Time
     int * seconds_pointer;
 
     public:
+    Time();
     Time(int);
+    ~Time();
 
 };
+
+Time::Time()
+{
+    seconds_pointer = new int;
+    seconds_pointer * = 0;
+}
 
 Time::Time(int seconds)
 {
     seconds_pointer = new int;
     seconds_pointer* = seconds;
+}
+Time::~Time()
+{
+    delete seconds_pointer;
 }
 
 
