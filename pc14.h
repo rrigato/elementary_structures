4fcbdb87ad1b7c472bab809f6e8bfec1a14c810c
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-04-19 15:50:49 -0700

    Create pc14.h

---
+++ pc14.h
@@ -0,0 +1,23 @@
+#ifndef
+#define
+TIME_H
+class Time
+{
+
+    private:
+    int * seconds_pointer;
+
+    public:
+    Time(int);
+
+};
+
+Time::Time(int seconds)
+{
+    seconds_pointer = new int;
+    seconds_pointer* = seconds;
+}
+
+
+
+#endif
