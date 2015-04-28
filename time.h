0dd0caffffa679868896e27e828d8b4b8350e830
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-04-28 12:35:21 -0700

    Update time.h

--- time.h
+++ time.h
@@ -2,18 +2,18 @@
 #define TIME_H
 #include <iostream>
 using namespace std;
- 
- 
+
+
 //forward declarations for overloaded stream operators
 class Time;
 ostream & operator << ( ostream &, const Time & );
 istream & operator >> (istream & , const Time & );
 class Time
 {
-  
+ 
 private:
     int * seconds_pointer;
-  
+ 
 public:
     Time();
     Time(int);
@@ -37,29 +37,29 @@ public:
         *seconds_pointer = *(right.seconds_pointer);
     }
     ~Time();
-   
- 
+  
+
     Time operator ++ (int)
-    {
-          
+	{
+	     
         Time temp;
-        cout << *temp.seconds_pointer <<endl;
-        *(temp.seconds_pointer) = *seconds_pointer;
-        cout << *temp.seconds_pointer <<endl;       
+		cout << *temp.seconds_pointer <<endl;
+		*(temp.seconds_pointer) = *seconds_pointer;
+		cout << *temp.seconds_pointer <<endl;		
         ((*(temp.seconds_pointer)) ++);
-        cout << *temp.seconds_pointer <<endl;
- 
+		cout << *temp.seconds_pointer <<endl;
+
         return temp;
     }
+
  
-  
     double operator + (const Time &right)
     {
         double seconds_in_min = 60;
         return (*seconds_pointer + *(right.seconds_pointer)) / seconds_in_min;
-  
+ 
     }
-  
+ 
     Time(const Time & obj)
     {
         int temp = 0;
@@ -69,24 +69,24 @@ public:
     //  delete obj.seconds_pointer;
         *this = obj;
     }
- 
+
     friend istream & operator >>(istream &, const Time &);
     friend ostream & operator << (ostream &, const Time &);
 };
- 
+
 ostream & operator << ( ostream & stream, const Time & obj)
 {
         stream << *(obj.seconds_pointer);
         return stream;
-  
+ 
 }
-  
+ 
 istream & operator >> (istream & is, const Time & obj)
 {
     is >> *(obj.seconds_pointer);
     return is;
 }
-  
+ 
 double Time::getDays() const
 {
     double seconds_in_min = 60;
@@ -102,22 +102,22 @@ double Time::getMinutes()const
 int Time::getSeconds()const
 {
     return *seconds_pointer;
-  
+ 
 }
 double Time::getHours()const
 {
     double seconds_in_min = 60;
     double min_in_hour = 60;
-  
+ 
     return (*seconds_pointer / seconds_in_min) / min_in_hour;
 }
-  
+ 
 Time::Time()
 {
     seconds_pointer = new int;
     *seconds_pointer = 0;
 }
-  
+ 
 Time::Time(int seconds)
 {
     seconds_pointer = new int;
@@ -127,7 +127,7 @@ Time::~Time()
 {
     delete seconds_pointer;
 }
-  
  
- 
+
+
 #endif
