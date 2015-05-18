fa0265cb2dd432c5c8ce8bd79aa8681ab22f613e
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-18 11:26:53 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -15,6 +15,33 @@ public:
        {
               Head = NULL;
        }
+       Queue( Queue & obj)
+       {
+              Head = obj.Head
+              Node * helper1;
+              helper1 = obj.Head;
+              Node * helper2 = head;
+              while (helper1)
+              {
+                    helper2->next = helper1->next;
+              }
+       }
+       Queue & operator=(const Queue &obj)
+       {
+              if (&obj ==*this)
+               { return *this;}
+              Node * helper1 = Head;
+              Node * helper2 = obj.Head;
+              P value = helper2->data;
+              while(helper2)
+                 {
+                   helper1-> data = value;
+                   helper1 = helper1->next;
+                   helper2= helper2->next;
+                   value = helper2->data;
+                 }
+              return *this;
+       }
        
 };
 
