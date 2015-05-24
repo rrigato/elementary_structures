b8af72044800169eb3fc235f56d915fb7caeb280
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-24 16:58:34 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,5 +1,10 @@
 #ifndef MYQUEUE_H
 #define MYQUEUE_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
 template <class P>
 class Queue
 {
@@ -7,43 +12,124 @@ private:
         struct Node
         {
                P data;
-               Node * next;
+              Node * next;
         };
-        Node * Head;
+        Node * Front;
+        Node * Rear;
+        int num_nodes;
 public:
        Queue()
        {
-              Head = NULL;
+              Front = NULL;
        }
-       Queue( Queue & obj)
+       Queue & operator= (const &obj)
        {
-              Head = obj.Head
-              Node * helper1;
-              helper1 = obj.Head;
-              Node * helper2 = head;
-              while (helper1)
-              {
-                    helper2->next = helper1->next;
-              }
-       }
-       Queue & operator=(const Queue &obj)
-       {
-              if (&obj ==*this)
-               { return *this;}
-              Node * helper1 = Head;
-              Node * helper2 = obj.Head;
-              P value = helper2->data;
-              while(helper2)
-                 {
-                   helper1-> data = value;
-                   helper1 = helper1->next;
-                   helper2= helper2->next;
-                   value = helper2->data;
-                 }
-              return *this;
-       }
-       
+            if (&obj == this)
+            {
+
+                return this*;
+            }
+            if (obj->Front ==NULL)
+            {
+                return this*;
+            }
+            Node * helper = obj->Front;
+            while (helper)
+            {
+                enque(helper->data);
+                helper = helper->next;
+            }
+
+        }
+
+       void enque (P );
+       P deque ();
+       void menu( char) const;
+       void small_menu() const;
+
+
 };
 
+    template <class P>
+    void Queue<P>::small_menu() const
+    {
+        char choice;
+        cout << "(E)nque" <<endl;
+        cout << "(D)eque" <<endl;
+        cout << "(Q)uit" <<endl;
+        menu (choice);
+    }
+
+    template <class P>
+    void Queue<P>::menu(char letter) const
+    {
+        switch (letter)
+            {
+                case 'e':
+                    case 'E':  {
+                                    cout << "Enque the function " <<endl;
+                                    break;
+                               }
+                    case 'd':
+                    case 'D':
+                            {
+                                cout << "deque the function" <<endl;
+                                break;
+                            }
+                    case 'q':
+                    case 'Q':
+                            {
+                                cout << "bye bye " <<endl;
+                                return;
+                            }
+                    default:
+                            {
+                                cout << " Bad user hit a proper value "<<endl;
+                            }
+            }
+                small_menu();
+    }
+
+
+template <class P>
+P Queue<P>::deque()
+{
+    if (!Front)
+    {
+        throw "The queue is empty";
+    }
+    else
+    {
+        P value;
+        Node * helper1 = Front;
+        Front = Front->next;
+        value = helper1->data;
+        delete helper1;
+        return value;
+    }
+}
+ template <class P>
+ void Queue<P>::enque(P value)
+{
+      Node * NewNode;
+      NewNode = new Node;
+      NewNode->data = value;
+      Node * helper1 = Front;
+      if (!Front)
+      {
+                Front = NewNode;
+                Rear = NewNode;
+
+      }
+      else
+      {
+
+        Rear->next = NewNode;
+        Rear = NewNode;
+
+      }
+      NewNode->next = NULL;
+      num_nodes++;
+}
 
 #endif
