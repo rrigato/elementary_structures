50ae7c09163a80ef5390d4435363e1dd09612912
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-26 14:35:44 -0700

    Update stack.h

--- stack.h
+++ stack.h
@@ -7,59 +7,91 @@ template <class Z>
 class Stack
 {
 private:
-    struct Node
-    {
-        Z data;
-        Node* next;
-    };
-    Node * Top;
+	struct Node
+	{
+		Z data;
+		Node* next;
+	};
+	Node * Top;
 public:
-    Stack()
-    {
-        Top = 0;
-    }
-    Stack(Stack &obj)
-    {
-        *this = obj;
-    }
-    void push(Z);
-    Z pop();
-    bool isEmpty() const;
-    bool isFull() const;
-    Stack <Z> & operator=(Stack<Z> &obj)
-    {
+	Stack()
+	{
+		Top = 0;
+	}
+	Stack(Stack &obj)
+	{
+		*this = obj;
+	}
+	void push(Z);
+	Z pop();
+	bool isEmpty() const;
+	bool isFull() const;
+	void operator= (const Stack &obj)
+	{
+		if (obj.Top == NULL)
+		{
+			return;
+		}
+		if (Top != NULL)
+		{
+			while (Top)
+			{
+				pop();
+			}
+		}
+		if (&obj == *this)
+		{
+			return;
+		}
 
-        if (&obj == this)
-        {
-            return *this;
-        }
-        Z value;
-        //Node * helper1 = Top;
-        if (!obj.Top)
-        {
-            return *this;
+		else
+		{
+			Node * helper1;
+			helper1 = obj.Top;
+			while (helper1)
+			{
+				push(helper1->data);
+				helper1 = helper1->next;
+			}
 
-        }
-        else
-        {
+		}
 
-                Node * helper2 = obj.Top;
-        //helper2 = obj.Top;
-                Z temp;
-                while (helper2)
-                    {
-                    // value = helper2->data;
-                    temp = helper2->data;
-                    push(temp);
-                    //helper1->data = push(value);
+	}
 
-                    helper2 = helper2->next;
-                    // helper2 = helper2->next;
-
-                    }
-        }
-        return *this;
-    }
+	//    Stack <Z> & operator=(Stack<Z> &obj)
+	//    {
+	//
+	//        if (&obj == this)
+	//        {
+	//            return *this;
+	//        }
+	//        Z value;
+	//        //Node * helper1 = Top;
+	//        if (!obj.Top)
+	//        {
+	//            return *this;
+	//
+	//        }
+	//        else
+	//        {
+	//
+	//                Node * helper2 = obj.Top;
+	//        //helper2 = obj.Top;
+	//                Z temp;
+	//                while (helper2)
+	//                    {
+	//                    // value = helper2->data;
+	//                    temp = helper2->data;
+	//                    push(temp);
+	//                    //helper1->data = push(value);
+	//
+	//                    helper2 = helper2->next;
+	//                    // helper2 = helper2->next;
+	//
+	//                    }
+	//        }
+	//        return *this;
+	//    }
 
 };
 
@@ -67,55 +99,56 @@ public:
 template <class Z>
 bool Stack <Z>::isFull() const
 {
-    return false;
+	return false;
 }
 
 template <class Z>
 bool Stack <Z>::isEmpty()const
 {
-    if (!Top)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+	if (!Top)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 template <class Z>
 void Stack<Z>::push(Z value)
 {
-    Node * newNode;
-    newNode = new Node;
-    newNode->data = value;
-    if (!Top)
-    {
-        Top = newNode;
-    }
-    else
-    {
-        newNode->next = Top;
-        Top = newNode;
-    }
+	Node * newNode;
+	newNode = new Node;
+	newNode->data = value;
+	if (!Top)
+	{
+		Top = newNode;
+		newNode->next = NULL;
+	}
+	else
+	{
+		newNode->next = Top;
+		Top = newNode;
+	}
 }
 
 template <class Z>
 Z Stack<Z>::pop()
 {
-    if (!Top)
-    {
-        throw "Error: there are no values on the stack";
-    }
-    else
-    {
-        Z value;
-        value = Top->data;
-        Node * one = Top;
-        Top = Top->next;
-        delete one;
-        return value;
-    }
+	if (!Top)
+	{
+		throw "Error: there are no values on the stack";
+	}
+	else
+	{
+		Z value;
+		value = Top->data;
+		Node * one = Top;
+		Top = Top->next;
+		delete one;
+		return value;
+	}
 }
 
 
