a1e11d9e12ae69367f1e2b9103f6f5a43e611f3b
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-28 15:51:50 -0700

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -2,64 +2,31 @@
 #include <string>
 #include <cstring>
 using namespace std;
-void Reverse (string &);
- 
-int main ()
+void Reverse(string &);
+
+int main()
 {
-    string obj;
-    cout << "Write a string and I will reverse it for you" <<endl;
-    getline(cin, obj); //Gets the whole string until the user hits enter
-    Reverse(obj);
-    return 0;
+	string Obj;
+	cout << "Write a string and I will reverse it for you" << endl;
+	getline(cin, Obj); //Gets the whole string until the user hits enter
+	Reverse(Obj);
+	cin.get();
+	return 0;
 }
- 
-void Reverse (string & obj)
+
+void Reverse(string & Obj)
 {
 
-     if (obj.length() == 0)
-     {
-                return;
-     }
-     else
-     {
+	static int j = Obj.length() - 1;
+	if (j < 0)
+	{
+		return;
+	}
+	else
+	{
 
-        cout << obj[obj.length()-1];
-        string temp;
-    for (int i=0; i < obj.length()-2; i++)
-         {
-               temp[counter] = obj[counter];
-               counter++;
-         }
-         Reverse(temp);
-
-
-         
-        /* cout << obj[length-1]<<endl;
-         int counter = 0;
-         string temp;
-         while (counter <(length-2))
-         {
-               temp[counter] = obj[counter];
-               counter++;
-         }
-         Reverse(temp);*/
-     }   
+		cout << Obj[j] ;
+		j--;
+		Reverse(Obj);
+	}
 }
-/*
-void Reverse(string obj)
-{
-     char * c_string = new char[obj.length()+1];
-    strcpy (c_string, obj.c_str());
-     if (c_string[0]== '\0')
-     {
-          delete [] c_string; //deletes last c-string and then returns
-          return;
-     }
-     else
-     {
-         cout << c_string[0]
-          string temp
-     }
-     delete [] c_string; //deletes all other c_strings before function dies
-}
-*/
