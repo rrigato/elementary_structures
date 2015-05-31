fc775b373526a11f7c2e4c8f7b55f67f5fe15985
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-31 16:01:59 -0700

    Update Btree.h

--- Btree.h
+++ Btree.h
@@ -21,3 +21,56 @@ class Btree
         delete r;
         r=NULL;
     }
+    void seek(Node *r, Z data)
+    {
+        if (r==NULL)
+            return;
+        if(r->value == data)
+            Remove(r);
+        else if (r->value > data)
+        {
+            seek(r->Left, data);
+        }
+        else if(r->value < data)
+        {
+            seek(r->Right, data);
+        }
+ 
+    }
+    void Remove (Node *r)
+    {
+        if (r->Left ==NULL && r->Right ==NULL)
+        {
+            delete r;
+            r =NULL;
+            return;
+        }
+        Node * one = NULL;
+        if(r->Left !=NULL && r->Right == NULL)
+        {
+            one = r;
+            r = r->Left;
+            delete one;
+            return;
+        }
+        else if(r->Right != NULL && r->Left ==NULL)
+        {
+            one = r;
+            r = r->Right;
+            delete one;
+            return;
+        }
+        else
+        {
+            one = r->Right;
+            while(r->Left !=NULL)
+            {
+                one = one->Left;
+            }
+            one->Left = r->Left;
+            one = r;
+            r = r->Right;
+            delete one;
+            return;
+        }
+    }
