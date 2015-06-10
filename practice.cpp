b912bf5bde80c37ecb119a5ee37fd8ff5b1aaac0
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-10 10:13:43 -0700

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class linked_list
+{
+
+		struct Node
+		{
+			int data;
+			Node * Next;
+		};
+
+
+		Node * Head;
+
+
+		void insert( int );
+};
+
+void linked_list::insert( int value)
+{
+	Node * newNode = new Node;
+	newNode->data = value
+	if (!Head)
+
+	
+
+	
+	int main ()
+{
+	return 0;
+}
