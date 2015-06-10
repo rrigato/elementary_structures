8be642d1223def26ca7edd4e41ac0b020c471018
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-10 10:25:57 -0700

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//increasing list
 class linked_list
 {
 
@@ -19,13 +20,52 @@ class linked_list
 void linked_list::insert( int value)
 {
 	Node * newNode = new Node;
-	newNode->data = value
+	newNode->data = value;
 	if (!Head)
-
+	{
+		Head = newNode;
+		Head->Next = 0;
+		return;
+	}
+	else if (Head->Next ==0)
+	{
+		if (Head->data > value)
+		{
+			newNode->Next = Head;
+			Head = newNode;
+			
+		}
+		else 
+		{
+			Head->Next = newNode;
+			newNode->Next =0;
+		}
+		return;
+	}
+	else 
+	{
+		Node * one = Head;
+		Node * two = Head->Next;
+		for (; two != 0 && two->data < value; one = one->Next, two=two->Next);
+		if (two)
+		{
+			newNode->Next = two;
+			one->Next = newNode;
+			return;
+		}
+		else
+		{
+			one->Next = newNode;
+			newNode->Next =0;
+			return;
+		}
+	}
+}
 	
 
 	
 	int main ()
 {
+
 	return 0;
 }
