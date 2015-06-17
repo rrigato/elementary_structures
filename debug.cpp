45b94387505dfdbfde9403fa4e27ed23e688cde7
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-16 17:38:44 -0700

    Update debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
+void usage (char [] [20], int);
 int main ()
 {
   typedef char truth_table [10] [20];
+  truth_table name;
+  truth_table name2;
   std::ofstream debugFile;
   debugFile << "This is the debug information " <<endl;
   
   return 0;
+}
+
+void usage ( char name [] [20], int first_size)
+{
+  
 }
