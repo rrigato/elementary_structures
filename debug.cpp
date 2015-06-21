baf4a65d37fa8533ebe8ce1a5a59dd3e08d96920
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-21 12:46:55 -0700

    Update debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -3,10 +3,13 @@
 void usage (char [] [20], int);
 int main ()
 {
+  //example of a truth table
   typedef char truth_table [10] [20];
   truth_table name;
   truth_table name2;
   name2[1] [1] = FALSE;
+  name [5] [1] = TRUE;
+  
   usage (name2[] [1], 1);
   std::ofstream debugFile;
   debugFile << "This is the debug information " <<endl;
